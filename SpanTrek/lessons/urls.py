1db575454627bcb520d958784e99887fbce52157
BIGWOJ <wojtek.latos@interia.pl>
2025-08-29 13:46:13 +0200

    Added Peru and Mexico maps

--- SpanTrek/lessons/urls.py
+++ SpanTrek/lessons/urls.py
@@ -5,4 +5,7 @@ app_name = 'lessons'
 
 urlpatterns = [
     path('world-map/', views.world_map, name='world_map'),
+    path('spain/', views.spain_view, name='spain'),
+    path('peru/', views.peru_view, name='peru'),
+    path('mexico/', views.mexico_view, name='mexico'),
 ]
--- SpanTrek/lessons/views.py
+++ SpanTrek/lessons/views.py
@@ -1,40 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.db.models import Count
-from .models import Country, AdventureLesson
 
 
 @login_required
 def world_map(request):
-    """
-    World map page - displays interactive map with Spanish-speaking countries
-    """
-    # Get all Spanish-speaking countries
-    countries = Country.objects.all()
+   
+    return render(request, 'lessons/world_map.html')
+
+
+@login_required
+def spain_view(request):
     
-    # Calculate statistics for each country
-    country_stats = {}
-    for country in countries:
-        country_key = country.name.lower().replace(' ', '_')
-        total_lessons = AdventureLesson.objects.filter(country=country).count()
-        completed_lessons = AdventureLesson.objects.filter(
-            country=country,
-            userlesson__user=request.user,
-            userlesson__completed=True
-        ).distinct().count()
-        
-        country_stats[country_key] = {
-            'completed': completed_lessons,
-            'total': total_lessons
-        }
-    context = {
-        'page_title': 'World Map - Spanish Speaking Countries',
-        'country_stats': country_stats
-    }
+    return render(request, 'lessons/spain.html')
 
-    context = {
-        'page_title': 'World Map - Spanish Speaking Countries',
-        'country_stats': country_stats
-    }
-    return render(request, 'lessons/world_map.html', context)
+@login_required
+def peru_view(request):
 
+    return render(request, 'lessons/peru.html')
+
+
+@login_required
+def mexico_view(request):
+
+    return render(request, 'lessons/mexico.html')
+    
