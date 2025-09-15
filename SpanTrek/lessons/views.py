f19a99ebe26d87dbb694eb7053ff4f4838f863f4
BIGWOJ <wojtek.latos@interia.pl>
2025-09-15 22:47:41 +0200

    Learned words and sentences in lesson

--- SpanTrek/lessons/views.py
+++ SpanTrek/lessons/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Lesson
 
 
 @login_required
@@ -21,15 +22,20 @@ def country_city_lesson(request, country, city, lesson_number=None):
     # If lesson_number is not provided, use the current progress
     current_lesson = lesson_number if lesson_number is not None else city_progress
 
+    lesson = Lesson.objects.filter(city=city, order=current_lesson).first()
+
     context = {
         'city': city,
         'country': country,
+        'lesson': lesson,
+        'lesson_vocabularies': lesson.vocabularies.all(),
+        'lesson_sentences': lesson.sentences.all(),
         'lesson_number': current_lesson,
         'prev_lesson_number': current_lesson - 1 if current_lesson >= 1 else None,
         'next_lesson_number': current_lesson + 1 if current_lesson < 3 else None,
-        
     }
-    print(request.GET)
+
     template_name = f'lessons/{country}/{city}/lesson_{current_lesson}.html'
 
     return render(request, 'lessons/lesson_base.html', context)
+
